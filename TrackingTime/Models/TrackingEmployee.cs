a9cc196da9bead116e8848ec3d507c8f8250f603
PABLO EMILIO VILLA PEREZ <pablovilla266477@correo.itm.edu.co>
2021-09-02 14:15:49 -0500

    add response, model and entity

---
+++ TrackingTime/Models/TrackingEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trackingtime.Common.Models
+{
+    public class TrackingEmployee
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ trackingtime.Functions/Entities/TrackingEmployeeEntity.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trackingtime.Functions.Entities
+{
+    public class TrackingEmployeeEntity : TableEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
